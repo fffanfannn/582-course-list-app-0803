ac6a757089515a0eb88b3eb5d87631b3bf017363
fanfan <2295467@edu.vaniercollege.qc.ca>
2023-08-02 15:39:33 -0400

    test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <h1>Course List App</h1>
+  <p>{{ count }}</p>
+  <CourseItem v-bind:v-for="courselist in courselists" :given="courselist" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import CourseItem from "./components/CourseItem.vue";
 
 export default {
   name: "App",
+
   components: {
-    HelloWorld,
+    CourseItem,
+  },
+
+  data() {
+    return {
+      count: 0,
+      courselists: [
+        { id: 1, name: "AA", age: 15 },
+        { id: 2, name: "BB", age: 17 },
+      ],
+    };
+  },
+
+  methods: {
+    addOne() {
+      this.count++;
+    },
   },
 };
 </script>
---
+++ src/components/CourseItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>Course Name</div>
+  <p>{{ count }}</p>
+  <p>{{ given.name }} - {{ given.id }} -{{ given.age }}</p>
+  <button @click="innerAdd">Add</button>
+  <button @click="outerAdd">Add to parent</button>
+</template>
+
+<script>
+export default {
+  name: "CourseItem",
+  props: {
+    given: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    innerAdd() {
+      this.count++;
+    },
+    outerAdd() {
+      this.$emit("count-to-parent");
+    },
+  },
+};
+</script>
